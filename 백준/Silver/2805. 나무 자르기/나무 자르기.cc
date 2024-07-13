c0393a595fccbf76606be013e33fe7075da3d006
Jehee Cheon <jeheecheon@gmail.com>
2024-07-13 23:56:36 +0900

    [Silver II] Title: 나무 자르기, Time: 576 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+using int64 = long long;
+
+int N, M;
+int logs[1000002];
+
+bool solve(int64 mid)
+{
+    int64 ans = 0;
+    for (int i = 0; i < N; ++i) {
+        if (logs[i] <= mid)
+            continue;
+
+        ans += logs[i] - mid;
+        if (ans >= M)
+            return true;
+    }
+    return false;
+}
+
+int main(void)
+{
+    cin >> N >> M;
+    for (int i = 0; i < N; ++i)
+        cin >> logs[i];
+
+    int64 st = 0, en = 1e9;
+    int ans = 0;
+
+    while (st <= en) {
+        int64 mid = (st + en) / 2;
+
+        if (solve(mid)) {
+            st = mid + 1;
+            ans = mid;
+        }
+        else {
+            en = mid - 1;
+        }
+    }
+
+    cout << en;
+}
