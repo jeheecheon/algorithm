369faa5d2251b963c604085992f040399f27f51f
Jehee Cheon <jeheecheon@gmail.com>
2024-07-20 10:46:16 +0900

    [Gold V] Title: 수강 과목, Time: 8 ms, Memory: 2184 KB -BaekjoonHub

---
+++ 백준/Gold/17845. 수강 과목/수강 과목.cc
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+    cin.tie(0)->tie(0)->sync_with_stdio(0);
+
+    int N, T;
+    cin >> T >> N;
+    vector<pair<int, int>> courses(N);
+    for (int i = 0; i < N; ++i)
+        cin >> courses[i].second >> courses[i].first;
+    vector<int> dp(T + 1);
+    for (int i = 0; i < N; ++i)
+        for (int j = T; j >= courses[i].first; --j)
+            dp[j] = max(
+                dp[j],
+                dp[j - courses[i].first] + courses[i].second);
+
+    cout << dp[T];
+}
