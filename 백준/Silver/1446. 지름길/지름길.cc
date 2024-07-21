8304b53450b91d3f516bd829f4c24d933bbe840d
Jehee Cheon <jeheecheon@gmail.com>
2024-07-21 23:39:19 +0900

    [Silver I] Title: 지름길, Time: 0 ms, Memory: 2260 KB -BaekjoonHub

---
+++ 백준/Silver/1446. 지름길/지름길.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct myComp {
+    bool operator()(pair<int, int> f, pair<int, int> s)
+    {
+        return f > s;
+    }
+};
+
+int main()
+{
+    int N, D;
+    cin >> N >> D;
+
+    vector<int> dist(10001, 0x7fffff);
+    vector<vector<pair<int, int>>> roads(10001);
+
+    for (int i = 0; i < D; ++i)
+        roads[i].emplace_back(i + 1, 1);
+
+    for (int i = 0; i < N; ++i) {
+        int st, end, len;
+        cin >> st >> end >> len;
+        roads[st].emplace_back(end, len);
+    }
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, myComp> PQ;
+    PQ.push({ 0, 0 });
+    dist[0] = 0;
+
+    while (!PQ.empty()) {
+        auto [st, curLength] = PQ.top();
+        PQ.pop();
+
+        if (curLength > dist[st])
+            continue;
+
+        for (auto [nxtDest, weight] : roads[st]) {
+            if (nxtDest <= D && weight + dist[st] < dist[nxtDest]) {
+                dist[nxtDest] = dist[st] + weight;
+                PQ.push({ nxtDest, dist[nxtDest] });
+            }
+        }
+    }
+
+    cout << dist[D];
+}
