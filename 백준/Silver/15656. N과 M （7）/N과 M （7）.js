926eb498924552afc722303d538831aafe90806a
Jehee Cheon <jeheecheon@gmail.com>
2024-10-05 01:45:57 +0900

    [Silver III] Title: N과 M (7), Time: 932 ms, Memory: 185812 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.js
@@ -0,0 +1,38 @@
+/**
+ * Input
+ */
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./src/input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number);
+
+/**
+ * Solution
+ */
+arr.sort((a, b) => a -b);
+let ans = "";
+const vis = Array(8).fill(false);
+const picked = [-1];
+const solve = (k) => {
+    if (k == M) {
+        ans += picked.slice(1).map(e => arr[e]).join(" ") + "\n";
+        return;
+    }
+
+    for (let i = 0; i < N; ++i) {
+        // if (vis[i])
+        //     continue;
+
+        // vis[i] = true;
+        picked.push(i);
+
+        solve(k + 1);
+
+        // vis[i] = false;
+        picked.pop();
+    }
+};
+
+solve(0);
+console.log(ans);
