063243821b80e6d6637c4c33aa7a1def49e7d714
Jehee Cheon <jeheecheon@gmail.com>
2024-10-11 03:41:06 +0900

    [Silver IV] Title: 수 찾기, Time: 292 ms, Memory: 41960 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -0,0 +1,35 @@
+/**
+ * Input
+ */
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./src/input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+let arr1 = input[1].split(" ").map(Number);
+let arr2 = input[3].split(" ").map(Number);
+
+/**
+ * Solution
+ */
+const ans = [];
+arr1 = arr1.sort((a, b) => a - b);
+
+arr2.forEach((e) => {
+    let st = 0,
+        end = arr1.length - 1;
+    
+    let found = false;
+    while (st <= end) {
+        let mid = Math.trunc((st + end) / 2);
+
+        if (arr1[mid] < e) 
+            st = mid + 1;
+        else if (arr1[mid] > e) 
+            end = mid - 1;
+        else {
+            found = true;
+            break;
+        }
+    }
+    ans.push(found ? 1 : 0);
+});
+console.log(ans.join("\n"));
